39355c05b65f6c3157e291a638b4a54535ecd3b9
SalvaWebPr0 <salvawebpro@gmail.com>
2024-02-23 11:07:46 -0600

    update store to server actions

--- src/app/[lang]/catalog/page.jsx
+++ src/app/[lang]/catalog/page.jsx
@@ -1,47 +1,51 @@
 import React from 'react';
 import ProductCatalog from '@/components/productComponents/productCatalog/ProductCatalog';
-import queryString from 'query-string';
-import PaginationControllerComponent from '@/components/pagination/PaginationComponent';
 import MobileFilterComponet from '@/components/productComponents/productCatalog/mobileFilter/MobileFilterComponet';
 import InnerSectionTextComponent from '@/components/texts/InnerSectionTextComponent';
+import { getAllProduct } from '../_actions';
+import ServerPagination from '@/components/pagination/ServerPagination';
 
-const getProducts = async (searchParams) => {
-  const BASE_URL = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/products`;
+const CatalogPage = async ({ searchParams, params: { lang } }) => {
   const urlParams = {
     keyword: searchParams.keyword,
+    page: searchParams.page,
     category: searchParams.category,
     brand: searchParams.brand,
   };
-
-  const searchQuery = queryString.stringify(urlParams);
-  const res = await fetch(`${BASE_URL}?${searchQuery}`, { cache: 'no-store' });
-  const docs = await res.json();
-
-  let data = docs;
-
-  return data;
-};
-
-const CatalogPage = async ({ searchParams, params: { lang } }) => {
-  const productsData = await getProducts(searchParams);
-  const page = searchParams['page'] ?? '1';
-  const per_page = searchParams['per_age'] ?? '8';
-  const current_category = searchParams['category'] ?? '& stylish';
-  const start = (Number(page) - 1) * Number(per_page); // 0, 5, 10 ...
-  const end = start + Number(per_page); // 5, 10, 15 ...
-
-  let entries = productsData.products;
-  let allCategories = productsData.allCategories;
-  let allBrands = productsData.allBrands;
+  console.log(urlParams);
+  const filteredUrlParams = Object.fromEntries(
+    Object.entries(urlParams).filter(([key, value]) => value !== undefined)
+  );
+  console.log(filteredUrlParams);
+  const searchQuery = new URLSearchParams(filteredUrlParams).toString();
+  const data = await getAllProduct(searchQuery);
+  let entries = JSON.parse(data.products);
+  let allCategories = JSON.parse(data.allCategories);
+  let allBrands = JSON.parse(data.allBrands);
 
   const totalProductCount = entries.length;
 
-  entries = entries.slice(start, end);
+  // pagination
+  let page = parseInt(searchParams.page, 10);
+  page = !page || page < 1 ? 1 : page;
+  const perPage = 12;
+  const itemCount = data?.productsCount;
+  const totalPages = Math.ceil(data.filteredProductsCount / perPage);
+  const prevPage = page - 1 > 0 ? page - 1 : 1;
+  const nextPage = page + 1;
+  const isPageOutOfRange = page > totalPages;
+  const pageNumbers = [];
+  const offsetNumber = 1;
+  for (let i = page - offsetNumber; i <= page + offsetNumber; i++) {
+    if (i >= 1 && i <= totalPages) {
+      pageNumbers.push(i);
+    }
+  }
   return (
     <div className="flex flex-col">
       <div className="py-7" />
       <InnerSectionTextComponent
-        title={`Amazing ${current_category} dresses for a special occasion.`}
+        title={`Amazing dresses for a special occasion.`}
         paraOne={`Explore our wide variety of designers and styles, find the perfect dress for any occasion`}
       />
       <div className="flex flex-col">
@@ -55,12 +59,15 @@ const CatalogPage = async ({ searchParams, params: { lang } }) => {
         <ProductCatalog data={entries} lang={lang} />
       </div>
       <div className="mt-5" />
-      <PaginationControllerComponent
-        hasNextPage={end < totalProductCount}
-        hasPrevPage={start > 0}
-        totalProductCount={totalProductCount}
-        lang={lang}
+      <ServerPagination
+        isPageOutOfRange={isPageOutOfRange}
+        page={page}
+        pageNumbers={pageNumbers}
+        prevPage={prevPage}
+        nextPage={nextPage}
+        totalPages={totalPages}
       />
+
       <div className="mb-5" />
     </div>
   );
--- src/lib/APIFilters.js
+++ src/lib/APIFilters.js
@@ -5,28 +5,25 @@ class APIFilters {
   }
 
   searchAllFields() {
+    console.log(this.queryStr);
     const keyword = this.queryStr.get('keyword');
     // Define the conditions to search for the keyword in title, description, and category
     const searchConditions = {
       $or: [
         { title: { $regex: keyword, $options: 'i' } },
-        //{ description: { $regex: keyword, $options: 'i' } },
+        { description: { $regex: keyword, $options: 'i' } },
         { category: { $regex: keyword, $options: 'i' } },
         { brand: { $regex: keyword, $options: 'i' } },
       ],
     };
 
-    // Combine keyword search with other existing query conditions
-    const combinedConditions = keyword
-      ? {
-          $and: [
-            searchConditions, // Keyword search conditions
-          ],
-        }
-      : this.query; // If no keyword, keep existing conditions
+    // Use a temporary variable to hold the conditions
+    const tempConditions = keyword
+      ? { $and: [this.query._conditions || {}, searchConditions] }
+      : this.query._conditions; // If no keyword, keep existing conditions
 
-    // Apply the combined conditions to your query
-    this.query = this.query.find(combinedConditions);
+    // Set the conditions to this.query._conditions
+    this.query._conditions = tempConditions;
 
     return this;
   }
@@ -40,16 +37,14 @@ class APIFilters {
     const removeFields = ['keyword', 'page', 'per_page'];
     removeFields.forEach((el) => delete queryCopy[el]);
     let prop = '';
-    // Price Fitler for gt> gte>= lt< lte<= in PRICE
+    //Price Filter for gt> gte>= lt< lte<= in PRICE
     let output = {};
     for (let key in queryCopy) {
       if (!key.match(/\b(gt|gte|lt|lte)/)) {
         output[key] = queryCopy[key];
       } else {
         prop = key.split('[')[0];
-
         let operator = key.match(/\[(.*)\]/)[1];
-
         if (!output[prop]) {
           output[prop] = {};
         }
@@ -62,9 +57,7 @@ class APIFilters {
     return this;
   }
 
-  pagination(resPerPage) {
-    const currentPage = Number(this.queryStr.get('page')) || 1;
-
+  pagination(resPerPage, currentPage) {
     const skip = resPerPage * (currentPage - 1);
 
     this.query = this.query.limit(resPerPage).skip(skip);
